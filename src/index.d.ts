7752b84c5dfd524b23a7381a20638439092a61cb
nivk <nivk@math3d.xyz>
2025-08-11 00:12:08 +0800

    fix: 修复提示类型错误

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,13 +7,13 @@ declare class ReadOnlyList<T> {
   /**
    * 获取包含的元素数
    */
-  get length(): Number;
+  get length(): number;
 
   /**
    * 获取指定元素类型的 List 类型
-   * @param innerType 元素类型
+   * @param innerType 元素类型构造函数
    */
-  static T<T>(innerType: T): typeof List<T>;
+  static T<T>(innerType: any): typeof List<T>;
 
   /**
    * 合并两个 List
@@ -33,7 +33,7 @@ declare class ReadOnlyList<T> {
    * @param item 要判断的元素
    * @returns 是否包含
    */
-  contains(item: T): Boolean;
+  contains(item: T): boolean;
 
   /**
    * 复制到一个数组
@@ -46,7 +46,7 @@ declare class ReadOnlyList<T> {
    * @param array 目标数组
    * @param arrayIndex 数组起始位置
    */
-  copyTo(array: T[], arrayIndex: Number): void;
+  copyTo(array: T[], arrayIndex: number): void;
 
   /**
    * 复制到一个数组
@@ -54,14 +54,14 @@ declare class ReadOnlyList<T> {
    * @param arrayIndex 数组起始位置
    * @param count 复制数量
    */
-  copyTo(array: T[], arrayIndex: Number, count: Number): void;
+  copyTo(array: T[], arrayIndex: number, count: number): void;
 
   /**
    * 确定 List 中是否包含与指定谓词定义的条件匹配的元素
    * @param predicate 判断函数
    * @returns 是否包含
    */
-  exists(predicate: Function): Boolean;
+  exists(predicate: Function): boolean;
 
   /**
    * 对 List 中每个元素执行指定操作
@@ -81,7 +81,7 @@ declare class ReadOnlyList<T> {
    * @param predicate 判断函数
    * @returns 符合条件的元素索引
    */
-  findIndex(predicate: Function): Number;
+  findIndex(predicate: Function): number;
 
   /**
    * 搜索与指定谓词所定义的条件相匹配的元素，并返回整个 List 中的最后一个匹配元素
@@ -95,7 +95,7 @@ declare class ReadOnlyList<T> {
    * @param predicate 判断函数
    * @returns 符合条件的元素索引
    */
-  findLastIndex(predicate: Function): Number;
+  findLastIndex(predicate: Function): number;
 
   /**
    * 获取内部元素类型
@@ -108,14 +108,14 @@ declare class ReadOnlyList<T> {
    * @param item 要搜索的元素
    * @returns 元素索引
    */
-  indexOf(item: T): Number;
+  indexOf(item: T): number;
 
   /**
    * 搜索指定的对象，并返回整个 List 中最后一个匹配项的从零开始的索引
    * @param item 要搜索的元素
    * @returns 元素索引
    */
-  lastIndexOf(item: T): Number;
+  lastIndexOf(item: T): number;
 
   /**
    * 在源 List 中创建元素范围的浅表副本
@@ -123,7 +123,7 @@ declare class ReadOnlyList<T> {
    * @param length 复制数量
    * @returns 新 List
    */
-  slice(start: Number, length: Number): List<T>;
+  slice(start: number, length: number): List<T>;
 
   /**
    * 将整个 List 中的元素复制到新数组中
@@ -136,13 +136,13 @@ declare class ReadOnlyList<T> {
    * @param predicate 判断函数
    * @returns 是否所有元素都符合条件
    */
-  trueForAll(predicate: Function): Boolean;
+  trueForAll(predicate: Function): boolean;
 
   /**
    * 返回 List 的字符串表示形式
    * @returns 字符串
    */
-  toString(): String;
+  toString(): string;
 }
 
 /**
@@ -151,23 +151,23 @@ declare class ReadOnlyList<T> {
 export default class List<T> extends ReadOnlyList<T> {
   /**
    * 初始化 List 类的新实例
-   * @param innerType 元素类型
+   * @param innerType 元素类型构造函数
    */
-  constructor(innerType: T);
+  constructor(innerType: any);
 
   /**
    * 初始化 List 类的新实例
-   * @param innerType 元素类型
+   * @param innerType 元素类型构造函数
    * @param list 要复制的元素列表
    */
-  constructor(innerType: T, list: T[] | List<T>);
+  constructor(innerType: any, list: T[] | List<T>);
 
   /**
    * 初始化 List 类的新实例
-   * @param innerType 元素类型
+   * @param innerType 元素类型构造函数
    * @param count 初始容量
    */
-  constructor(innerType: T, count: Number);
+  constructor(innerType: any, count: number);
 
   /**
    * 添加一个元素
@@ -203,48 +203,48 @@ export default class List<T> extends ReadOnlyList<T> {
    * @param index 索引
    * @param item 要插入的元素
    */
-  insert(index: Number, item: T | null): void;
+  insert(index: number, item: T | null): void;
 
   /**
    * 将集合的元素插入 List 的指定索引处
    * @param index 索引
    * @param list 要插入的元素列表
    */
-  insertRange(index: Number, list: T[] | List<T>): void;
+  insertRange(index: number, list: T[] | List<T>): void;
 
   /**
    * 将集合的元素插入 List 的指定索引处
    * @param index 索引
    * @param items 要插入的元素列表
    */
-  insertRange(index: Number, ...items: T[]): void;
+  insertRange(index: number, ...items: T[]): void;
 
   /**
    * 从 List 中移除特定对象的第一个匹配项
    * @param item 要移除的元素
    * @returns 是否成功移除
    */
-  remove(item: T): Boolean;
+  remove(item: T): boolean;
 
   /**
    * 移除 List 中指定索引处的元素
    * @param index 索引
    */
-  removeAt(index: Number): void;
+  removeAt(index: number): void;
 
   /**
    * 删除与指定谓词所定义的条件匹配的所有元素
    * @param predicate 判断函数
    * @returns 删除数量
    */
-  removeAll(predicate: Function): Number;
+  removeAll(predicate: Function): number;
 
   /**
    * 移除 List 中指定索引处的元素
    * @param index 索引
    * @param count 移除数量
    */
-  removeRange(index: Number, count: Number): void;
+  removeRange(index: number, count: number): void;
 
   /**
    * 反转整个 List 中元素的顺序
@@ -256,7 +256,7 @@ export default class List<T> extends ReadOnlyList<T> {
    * @param index 起始索引
    * @param count 反转数量
    */
-  reverse(index: Number, count: Number): void;
+  reverse(index: number, count: number): void;
 
   /**
    * 在源 List 中创建元素范围的浅表副本
@@ -264,7 +264,7 @@ export default class List<T> extends ReadOnlyList<T> {
    * @param length 复制数量
    * @returns 新 List
    */
-  slice(start: Number, length: Number): List<T>;
+  slice(start: number, length: number): List<T>;
 
   /**
    * 使用默认比较器对整个 List 中的元素进行排序
